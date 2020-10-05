543ed7e2f3fbb8f0dd627715e41f6303eb5e119b
bcampos9539 <bccampos@asu.edu>
2020-10-05 12:22:38 -0700

    initial checkin for assignment2

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) { // adds a number to the total
+  }
+
+  public void subtract (int value) { // subtracts a number from the total
+  }
+
+  public String toString () { 
+    return "";
+  }
+
+  public void clear() { 
+  }
+}
