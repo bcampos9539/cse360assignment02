1db726fc8cb01fdd9fd4110778db712d1e841168
bcampos9539 <bccampos@asu.edu>
2020-10-05 12:45:17 -0700

    finalized methods, added history string

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,36 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String equation = "0"; //history string
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () { //returns the total value
+    return total;
   }
   
-  public void add (int value) { // adds a number to the total
+  public void add (int value) { // adds a number to the total, then updates the toString string
+	  total += value;
+	  equation += " + " + value;
+	  
   }
 
-  public void subtract (int value) { // subtracts a number from the total
+  public void subtract (int value) { // subtracts a number from the total, then updates the toString string
+	  total -= value;
+	  equation += " - " + value;
   }
 
-  public String toString () { 
-    return "";
+  public String toString () { //returns the final string with all added changes to the total
+    return equation;
   }
 
-  public void clear() { 
+  public void clear() { //resets total and the history string
+	  total = 0;
+	  equation = "0";
   }
+  
+  
 }
+
